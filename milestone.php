f4380f465da44221ac9a210f3f006ccda34e88b8
oglacor <5513914+oglacor@users.noreply.github.com>
2026-07-15 10:14:42 -0700

    unpdated button to edit in mileston view in tabi modal

--- milestone.php
+++ milestone.php
@@ -22,6 +22,11 @@
                 <a class="action-button" href="<?= $permalink ; ?>"><?= __("Do it now!","bluerabbit"); ?></a>
             </div>
             <div class="milestone-modal-divider"></div>
+			<?php if($isGM || $isAdmin){ ?>
+				<a class="form-ui" href="<?= get_bloginfo("url")."/new-$mi->quest_type/?questID=$mi->quest_id&adventure_id=$adv_parent_id"; ?>">
+					<span class="icon icon-edit inline-block"></span>
+				</a>
+			<?php } ?>
             <h2 class="milestone-level"><?=$mi->quest_type; ?> <?= __("Level","bluerabbit"); ?>: <?= $mi->mech_level; ?></h2>
             <h2 class="milestone-xp"><?=$xp_label; ?>: <?= $mi->mech_xp; ?></h2>
             <h2 class="milestone-bloo"><?=$bloo_label; ?>: <?= $mi->mech_bloo; ?></h2>
